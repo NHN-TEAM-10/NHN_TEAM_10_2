79cf19fe4ca32d8b55c651c994547a3315f5dc41
Imheroman <duddnd4457@naver.com>
2023-10-18 13:59:09 +0900

    young java code push in develop ,,,, last time :):):)

--- src/main/java/young/thread/thread1_2/ThreadCounterMain.java
+++ src/main/java/young/thread/thread1_2/ThreadCounterMain.java
@@ -1,12 +1,14 @@
 package young.thread.thread1_2;
 
 public class ThreadCounterMain {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
         ThreadCounter threadCounter1 = new ThreadCounter("threadCounter1", 10);
-        ThreadCounter threadCounter2 = new ThreadCounter("threadCounter2", 10);
+//        ThreadCounter threadCounter2 = new ThreadCounter("threadCounter2", 10);
+        Counter counter = new Counter("Counter", 10);
 
         threadCounter1.start();
-        threadCounter2.start();
+        counter.run();
+//        threadCounter2.start();
 
 //        threadCounter1.run();
 //        threadCounter2.run();
