d4077507742e9e80348bf68f2d0595fa517ed452
김성현 <aruesin2@gmail.com>
2023-10-24 09:39:33 +0900

    Thread Ticker 시간측정 기능 추가

--- src/main/java/seong/thread/shareShread/Quiz01.java
+++ src/main/java/seong/thread/shareShread/Quiz01.java
@@ -1,8 +1,8 @@
 package seong.thread.shareShread;
 
 public class Quiz01 {
-    public static void main(String[] args) {
-        Ticker ticker = new Ticker("1000n");
+    public static void main(String[] args) throws InterruptedException {
+        Ticker ticker = new Ticker("1000");
         ticker.start();
     }
 }
