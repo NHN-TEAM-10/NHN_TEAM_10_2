f69401a685fa8730675d01fa95e90697282089c8
김성현 <aruesin2@gmail.com>
2023-10-24 19:10:52 +0900

    생산자- 소비자 솔루션

--- src/main/java/seong/thread/ThreadStore/StoreTest.java
+++ src/main/java/seong/thread/ThreadStore/StoreTest.java
@@ -6,13 +6,11 @@ public class StoreTest {
     public static void main(String[] args) throws InterruptedException {
         Store store = new Store();
 
-        // Create and start Seller threads
         for (int i = 0; i < 2; i++) {
             Seller seller = new Seller(store);
             seller.start();
         }
 
-        // Create and start Buyer threads
         for (int i = 0; i < 5; i++) {
             Buyer buyer = new Buyer( String.valueOf(i), store);
             buyer.start();
