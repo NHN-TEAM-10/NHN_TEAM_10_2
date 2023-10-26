2b3641dfa91fe574f7490c4980668931a191e07b
rkdrjsrks <rkdrjs71@naver.com>
2023-10-26 09:56:56 +0900

    Thread 소비자 - 생산자 commit

---
+++ src/main/java/gun/javaThreadProgramming/state/Exam02.java
@@ -0,0 +1,10 @@
+package gun.javaThreadProgramming.state;
+
+public class Exam02 {
+    public static void main(String[] args) {
+        RunnableCounter runnableCounter = new RunnableCounter("counter", 5);
+
+        runnableCounter.start();
+        System.out.println(runnableCounter.getThread().getState());
+    }
+}
