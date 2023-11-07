3a3b6be255d3df443e56995ada27ca9a82e95768
김성현 <aruesin2@gmail.com>
2023-11-07 16:39:50 +0900

    Course 3 완료.

--- src/main/java/seong/course3/Exercise5.java
+++ src/main/java/seong/course3/Exercise5.java
@@ -7,17 +7,19 @@ import java.io.IOException;
 
 public class Exercise5 {
     public static void main(String[] args) throws IOException {
-        File file = new File("sales.dat");
+        File file = new File(
+                "/Users/gimseonghyeon/Desktop/maven-test/NHN_TEAM_10_2/src/main/java/seong/course3/sales.dat");
         BufferedReader br = new BufferedReader(new FileReader(file));
 
         double result = 0;
         String line;
-        while((line = br.readLine()) != null){
+        int notProvidedCount = 0;
+        while ((line = br.readLine()) != null) {
             int index = 0;
             while (index < line.length()) {
                 if (line.charAt(index) != ':') {
-                       index++;
-                }else{
+                    index++;
+                } else {
                     break;
                 }
             }
@@ -28,11 +30,12 @@ public class Exercise5 {
                 value = Double.parseDouble(line);
                 result += value;
             } catch (NumberFormatException e) {
+                notProvidedCount++;
             }
 
         }
-        System.out.println(result);
-
+        System.out.println("매출 총합 : " + result);
+        System.out.println("제공되지 않은 도시 개수 : " + notProvidedCount);
 
 
     }
